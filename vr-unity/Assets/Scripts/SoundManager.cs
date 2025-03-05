f8f2572acc9ca6731444754d819c92518143d0b6
zhenyadolgikh <apple.ijenny@gmail.com>
2025-03-05 17:44:48 +0100

    small update

--- vr-unity/Assets/Scripts/SoundManager.cs
+++ vr-unity/Assets/Scripts/SoundManager.cs
@@ -5,20 +5,16 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    // Arrays to hold different groups of game objects with audio sources
     public GameObject[] level1;
     public GameObject[] level2;
     public GameObject[] level3;
 
-    // Buttons to trigger sound play
     public Button playSoundButton1;
     public Button playSoundButton2;
     public Button playSoundButton3;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Ensure the buttons are set and add listeners to each button
         if (playSoundButton1 != null)
         {
             playSoundButton1.onClick.AddListener(() => OnPlaySoundButtonClicked(level1));
@@ -35,23 +31,19 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    // This function will be triggered when any button is clicked
     void OnPlaySoundButtonClicked(GameObject[] soundGroup)
     {
-        // Play the sounds of the selected sound group
         foreach (GameObject obj in soundGroup)
         {
-            // Ensure the object has an AudioSource component
+   
             AudioSource audioSource = obj.GetComponent<AudioSource>();
             if (audioSource != null)
             {
-                // Play the sound at the object's position
                 audioSource.Play();
             }
         }
     }
 
-    // Optionally, add a function to stop all sounds from all groups
     public void StopAllSounds()
     {
         StopSoundGroup(level1);
@@ -59,7 +51,6 @@ public class SoundManager : MonoBehaviour
         StopSoundGroup(level3);
     }
 
-    // Function to stop sounds in a specific group
     void StopSoundGroup(GameObject[] soundGroup)
     {
         foreach (GameObject obj in soundGroup)

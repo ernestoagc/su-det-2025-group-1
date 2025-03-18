3477c67e7bcf723ac7c8627495c820fcd9c31bd9
zhenyadolgikh <apple.ijenny@gmail.com>
2025-03-18 21:28:12 +0100

    light and sound buttons are functional now

--- vr-unity/Assets/Scripts/LightManager.cs
+++ vr-unity/Assets/Scripts/LightManager.cs
@@ -7,102 +7,92 @@ using UnityEngine.UI;
 
 public class LightManager : MonoBehaviour
 {
-    public GameObject[] level1Lights;
-    public GameObject[] level2Lights;
-    public GameObject[] level3Lights;
+    public GameObject[] lights1;
+    public GameObject[] lights2;
+    public GameObject[] lights3;
 
     public Button lightButton1;
     public Button lightButton2;
     public Button lightButton3;
 
-    private Button activeButton = null;
-    private GameObject[] activeLightGroup = null;
+    private bool isLights1On = false;
+    private bool isLights2On = false;
+    private bool isLights3On = false;
 
     void Start()
     {
         if (lightButton1 != null)
-        {
-            lightButton1.onClick.AddListener(() => OnLightButtonClicked(lightButton1, level1Lights));
-        }
+            lightButton1.onClick.AddListener(() => OnButtonClicked(1));
 
         if (lightButton2 != null)
-        {
-            lightButton2.onClick.AddListener(() => OnLightButtonClicked(lightButton2, level2Lights));
-        }
+            lightButton2.onClick.AddListener(() => OnButtonClicked(2));
 
         if (lightButton3 != null)
-        {
-            lightButton3.onClick.AddListener(() => OnLightButtonClicked(lightButton3, level3Lights));
-        }
+            lightButton3.onClick.AddListener(() => OnButtonClicked(3));
     }
 
-
-    void OnLightButtonClicked(Button clickedButton, GameObject[] lightGroup)
+    public void OnButtonClicked(int buttonIndex)
     {
-        bool isTurningOn = (activeButton != clickedButton); 
-   
+        if (buttonIndex == 1)
+            ToggleLights(lights1, ref isLights1On, lightButton1, lightButton2, lightButton3);
+        else if (buttonIndex == 2)
+            ToggleLights(lights2, ref isLights2On, lightButton2, lightButton1, lightButton3);
+        else if (buttonIndex == 3)
+            ToggleLights(lights3, ref isLights3On, lightButton3, lightButton1, lightButton2);
+    }
 
-        if (isTurningOn)
+    void ToggleLights(GameObject[] lightGroup, ref bool isOn, Button activeButton, Button button1, Button button2)
+    {
+        if (isOn)
         {
-            
-            if (activeLightGroup != null)
-            {
-              
-                ToggleLightGroup(activeLightGroup, false);
-            }
-
-           
-            activeButton = clickedButton;
-            activeLightGroup = lightGroup;
-            DisableOtherButtons(clickedButton);
-
-         
-            ToggleLightGroup(lightGroup, true);
+            SetLights(lightGroup, false);
+            EnableButtons(button1, button2);
         }
         else
         {
-           
-            activeButton = null;
-            activeLightGroup = null;
-            EnableAllButtons();
-         
-            ToggleLightGroup(lightGroup, false);
+            TurnOffAllLights();
+            SetLights(lightGroup, true);
+            DisableButtons(button1, button2);
         }
+        isOn = !isOn;
     }
 
-    
-    void ToggleLightGroup(GameObject[] lightGroup, bool state)
+    void SetLights(GameObject[] lightGroup, bool state)
     {
         foreach (GameObject obj in lightGroup)
         {
-            
-            LightFlickr flickerScript = obj.GetComponent<LightFlickr>();
-            if (flickerScript != null)
+            Light light = obj.GetComponent<Light>();
+            if (light != null)
             {
-                flickerScript.ToggleLight(state);
-            }
-
-              Light lightComponent = obj.GetComponent<Light>();
-            if (lightComponent != null)
-            {
-                lightComponent.enabled = state;
+                light.enabled = state;
             }
         }
     }
 
- 
-    void DisableOtherButtons(Button active)
+    public void TurnOffAllLights()
     {
-        if (lightButton1 != active) lightButton1.interactable = false;
-        if (lightButton2 != active) lightButton2.interactable = false;
-        if (lightButton3 != active) lightButton3.interactable = false;
+        SetLights(lights1, false);
+        SetLights(lights2, false);
+        SetLights(lights3, false);
+        EnableButtons(lightButton1, lightButton2, lightButton3);
     }
 
-   
-    void EnableAllButtons()
+    void DisableButtons(Button button1, Button button2)
     {
-        lightButton1.interactable = true;
-        lightButton2.interactable = true;
-        lightButton3.interactable = true;
+        button1.interactable = false;
+        button2.interactable = false;
+    }
+
+    void EnableButtons(Button button1, Button button2)
+    {
+        button1.interactable = true;
+        button2.interactable = true;
+    }
+
+    void EnableButtons(Button button1, Button button2, Button button3)
+    {
+        button1.interactable = true;
+        button2.interactable = true;
+        button3.interactable = true;
     }
 }
